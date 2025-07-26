cc9cde133b063f5c988dfc61ae288f534d5c257c
Christopher Kelvin <christopherkelvin100@gmail.com>
2025-07-26 13:32:00 +0300

    Modified the profile style

--- app/components/profile.tsx
+++ app/components/profile.tsx
@@ -8,7 +8,7 @@ export const Profile = () => {
   console.log(socialIcons);
   return (
     <Element name="Home" className="flex items-center justify-center ">
-      <div className="flex flex-row  bg-blue-400/30 w-[50%] p-4 rounded-lg shadow-lg overflow-hidden mt-44">
+      <div className="flex flex-row  bg-blue-400/30 w-[50%] p-8 rounded-lg shadow-lg overflow-hidden mt-44" data-aos= "fade-up">
         <img
           src={Image}
           alt="Christopher Kelvin Mndeme photo"
@@ -20,7 +20,7 @@ export const Profile = () => {
           data-aos="fade-up"
           data-aos-duration="5000"
         >
-          <h1 className="text-4xl font-bold">Christopher Kelvin Mndeme </h1>
+          <h1 className="text-4xl font-bold ">Christopher Kelvin Mndeme </h1>
           <p className="text-lg font-serif">
             I am a{""}
             <TypeAnimation
@@ -42,18 +42,23 @@ export const Profile = () => {
               className="font-outfit"
             />
           </p>
-          <p className="text-sm text-gray-500 w-[80%] mt-3 ">
-            I am a Full Stack Developer experienced in building complete web and
-            mobile applications. I work confidently across frontend and backend
-            with real-time features and responsive design. I also have
-            experience integrating APIs, AI tools, and managing scalable
-            projects end-to-end.
+          <p className="text-sm text-gray-500  mt-3 ">
+            I am a passionate and results-driven Full Stack Developer with
+            hands-on experience in building complete web and mobile applications
+            from scratch. I work confidently across both frontend and backend
+            technologies, implementing real-time features, interactive user
+            interfaces, and fully responsive design. I specialize in integrating
+            third-party APIs, leveraging AI tools for smarter functionality, and
+            managing scalable, high-performance projects from conception to
+            deployment. My approach blends clean code, user-centric design, and
+            modern development practices to deliver seamless digital
+            experiences.
           </p>
-          <div className="flex gap-5 mt-14">
+          <div className="flex gap-5 mt-14 w-full justify-center -ml-7">
             {socialIcons.map((item, i) => (
               <Link
                 to={item.path}
-                className="text-blue-600 border border-blue-600 animate-bounce transition-colors rounded-full h-10 w-10 flex items-center justify-center"
+                className="text-blue-600 border-solid border-t-4 border-b-4 border-blue-600 animate-bounce transition-colors rounded-full h-12 w-16 flex items-center justify-center"
               >
                 <item.Icon size={item.size} color={item.color} />
               </Link>
